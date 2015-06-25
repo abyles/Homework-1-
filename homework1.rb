b78095744d05de375b08650052bb4a67e88d6a9c
Adriana Byles <a.byles@yahoo.com>
2015-06-25 16:09:13 -0400

    Adding code

---
+++ homework1.rb
@@ -0,0 +1,31 @@
+CHEETAH_GIRLS = [
+  "Raven-Symone", 
+  "Adrienne Bailon",
+  "Sabrina Bryan", 
+  "Kiely Williams"
+]
+
+auditions = [
+  "Raven-Symone", 
+  "Usher",
+  "Wiz Khalifa",
+  "Adrienne Bailon",
+  "Hulk Hogan",
+  "Sabrina Bryan", 
+  "Diego Lugo",
+  "Kiely Williams", 
+  "Justin Timberlake"
+]
+
+panel = []
+
+
+
+
+auditions.each do |name|
+    if CHEETAH_GIRLS.include? name 
+        panel.push(name)
+    end
+end
+
+puts panel
